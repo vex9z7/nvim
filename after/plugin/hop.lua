daf2b31b570d36a50a1bc826b64f47163f6037a0
Yu Guo <yu.guo@mujin.co.jp>
2024-05-07 10:14:20 +0900

    add smoka7/hop.nvim

---
+++ after/plugin/hop.lua
@@ -0,0 +1,54 @@
+-- place this in one of your configuration file(s)
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+local hint_positions = require('hop.hint').HintPosition
+
+vim.keymap.set('n', '<leader>e',
+    function()
+        hop.hint_camel_case {
+            direction = directions.AFTER_CURSOR,
+            hint_position = hint_positions.END,
+        }
+    end,
+    { desc='hop forawrd to end of word' }
+    )
+
+vim.keymap.set("n", "<leader>ge",
+    function()
+        hop.hint_camel_case {
+            direction = directions.BEFORE_CURSOR,
+            hint_position = hint_positions.END,
+        }
+    end,
+    { desc='hop backward to end of word' }
+    )
+
+vim.keymap.set("n", "<leader>w",
+    function()
+        hop.hint_camel_case {
+            direction = directions.AFTER_CURSOR,
+            hint_position = hint_positions.BEGIN,
+        }
+    end,
+    { desc='hop forawrd to word' }
+    )
+
+vim.keymap.set("n", "<leader>b",
+    function()
+        hop.hint_camel_case {
+            direction = directions.BEFORE_CURSOR,
+            hint_position = hint_positions.BEGIN,
+        }
+    end,
+    { desc='hop backward to word' }
+    )
+
+vim.keymap.set("n", "<leader>k",
+    function() hop.hint_vertical { direction = directions.BEFORE_CURSOR } end,
+    { desc='hop up' }
+    )
+
+vim.keymap.set("n", "<leader>j",
+    function() hop.hint_vertical { direction = directions.AFTER_CURSOR } end,
+    { desc='hop down' }
+    )
--- lua/rito/packer.lua
+++ lua/rito/packer.lua
@@ -59,4 +59,13 @@ use {
     }
   end
 }
+
+use {
+  'smoka7/hop.nvim',
+  tag = '*',
+  config = function()
+    -- you can configure Hop the way you like here; see :h hop-config
+    require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+  end
+}
 end)
