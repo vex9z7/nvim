56009e7d36cc4960bd9dfe5b5d11f7af7e31e58b
Yu Guo <yu.guo@mujin.co.jp>
2024-05-07 10:13:52 +0900

    add which-key.nvim

--- lua/rito/packer.lua
+++ lua/rito/packer.lua
@@ -46,4 +46,17 @@ use {
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
 }
+
+use {
+  "folke/which-key.nvim",
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+    require("which-key").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
 end)
