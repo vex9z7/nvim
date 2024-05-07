9329f132581ad82d85d5d1ca0dd4130ae83a591f
Yu Guo <yu.guo@mujin.co.jp>
2024-05-07 18:20:05 +0900

    add astrocore

---
+++ lua/rito/lazy/astrocore.lua
@@ -0,0 +1,8 @@
+return {
+  "AstroNvim/astrocore",
+  lazy = false, -- disable lazy loading
+  priority = 10000, -- load AstroCore first
+  opts = {
+    -- set configuration options  as described below
+  },
+}
