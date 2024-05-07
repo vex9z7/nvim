e0f686d06d73ee4b6f809707fd0ca2ea2a7382f3
Yu Guo <yu.guo@mujin.co.jp>
2024-05-07 10:38:07 +0900

    add primitive nvim-surround

--- lua/rito/packer.lua
+++ lua/rito/packer.lua
@@ -68,4 +68,14 @@ use {
     require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
   end
 }
+
+use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+})
 end)
