8ae2a4e0372df70c24ab03d0f5a425ad12bf6b71
Yu Guo <yu.guo@mujin.co.jp>
2024-05-07 15:52:43 +0900

    use vim-better-whitespace to auto strip white spaces

---
+++ lua/rito/lazy/vim-better-whitespace.lua
@@ -0,0 +1,7 @@
+return { 'ntpeters/vim-better-whitespace',
+    config=function ()
+        vim.g.strip_whitespace_on_save = true
+        vim.g.strip_whitespace_confirm = false
+        vim.g.strip_only_modified_lines = false
+    end
+}
