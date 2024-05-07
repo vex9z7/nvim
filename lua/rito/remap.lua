c4b6cc743468bd28e36230e49e64abde8099a8c1
Yu Guo <yu.guo@mujin.co.jp>
2024-05-07 10:57:31 +0900

    merge undo settings

--- lua/rito/remap.lua
+++ lua/rito/remap.lua
@@ -1,1 +1,6 @@
+vim.g.mapleader = " "
+
+-- turn off highlights
+vim.keymap.set('n', '<esc><esc>', '<cmd>noh<CR>', {noremap = true})
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc="hahaha"})
--- lua/rito/set.lua
+++ lua/rito/set.lua
@@ -1,6 +1,6 @@
 vim.opt.guicursor = ""
 
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
@@ -9,16 +9,27 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+-- vim.opt.autoindent = true
+
+-- awalys use the clipboard
+vim.opt.clipboard:append("unnamedplus")
 
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
--- vim.opt.undodir = false
--- vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+-- save undo trees in files
+vim.opt.undodir = vim.env.HOME .. "/.config/nvim/undo"
+vim.opt.undofile = true
+
+-- number of undo saved
+vim.opt.undolevels=10000
+vim.opt.undoreload=10000
+
+-- vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.inccommand = "nosplit"
 
 vim.opt.termguicolors = true
 
@@ -29,5 +40,3 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
-
-vim.g.mapleader = " "
