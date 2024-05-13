a91c35255a1ef05c84914a3beabad73f41701170
Yu Guo <yu.guo@mujin.co.jp>
2024-05-13 15:04:22 +0900

    add which-key groups

--- lua/rito/lazy/which-key.lua
+++ lua/rito/lazy/which-key.lua
@@ -1,13 +1,28 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    config=function ()
+        local groups = {
+            ["<Leader><Leader>g"] = { name = "Git" },
+            ["<Leader><Leader>b"] = { name = "Buffers" },
+            ["<Leader><Leader>f"] = { name = "Find" },
+            ["<Leader><Leader>l"] = { name = "Language Tools" },
+            ["<Leader><Leader>p"] = { name = "Packages" },
+            ["<Leader><Leader>t"] = { name = "Terminal" },
+            ["<Leader><Leader>u"] = { name = "UI/UX" },
+        }
+        local wk = require("which-key")
+        for key,group in pairs(groups) do
+            wk.register( { [key] = group })
+        end
+    end,
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    }
 }
