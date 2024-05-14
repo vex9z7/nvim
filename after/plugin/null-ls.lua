1eb33679c42dd8ca22ebe50c2968ada7c7e6fb76
Yu Guo <yu.guo@mujin.co.jp>
2024-05-14 10:26:22 +0900

    enable auto formatting on save

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,15 +1,35 @@
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/MAIN.md
 local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-    sources = {
-        -- TODO: use repo root config file
-        formatting.prettierd,
-        formatting.eslint_d,
-        diagnostics.eslint_d,
-        -- null_ls.builtins.completion.spell,
-    },
+  sources = {
+    formatting.prettierd.with({
+      prefer_local = "node_modules/.bin",
+    }),
+    formatting.eslint_d,
+
+    diagnostics.eslint_d,
+
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({
+            async = false,
+            filter = function(c)
+              return c.name == "null-ls"
+            end,
+          })
+        end,
+      })
+    end
+  end,
 })
