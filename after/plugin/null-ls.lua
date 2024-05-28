3a6f01bac8b70aa4685c530b4d259f1937c14057
Yu Guo <yu.guo@mujin.co.jp>
2024-05-28 17:41:55 +0900

    fork qmllint config from null-ls

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -9,6 +9,40 @@ local code_actions = null_ls.builtins.code_actions
 local CSPELL_FALLBACK_CONFIG_PATH = vim.fn.expand("~/.config/nvim/null-ls-config-fallbacks/cspell.json")
 local CSPELL_FALLBACK_EXTRA_ARGS = { "--config", CSPELL_FALLBACK_CONFIG_PATH }
 
+function qmllintSourceFactory()
+	local h = require("null-ls.helpers")
+	local methods = require("null-ls.methods")
+
+	local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+
+	return h.make_builtin({
+		name = "qmllint",
+		meta = {
+			url = "https://doc-snapshots.qt.io/qt6-dev/qtquick-tools-and-utilities.html#qmllint",
+			description = "qmllint is a tool shipped with Qt that verifies the syntatic validity of QML files. It also warns about some QML anti-patterns.",
+		},
+		method = DIAGNOSTICS,
+		filetypes = { "qml" },
+		generator_opts = {
+			command = "qmllint",
+			args = { "$FILENAME" },
+			to_stdin = false,
+			format = "raw",
+			from_stderr = true,
+			to_temp_file = true,
+
+			on_output = h.diagnostics.from_errorformat(
+				table.concat({
+					"%trror: %f:%l:%c: %m",
+					"%tarning: %f:%l:%c: %m",
+				}, ","),
+				"qmllint"
+			),
+		},
+		factory = h.generator_factory,
+	})
+end
+
 null_ls.setup({
 	sources = {
 		-- lua
@@ -20,6 +54,20 @@ null_ls.setup({
 		}),
 		formatting.eslint_d,
 		diagnostics.eslint_d,
+
+		-- qml
+		formatting.qmlformat,
+		qmllintSourceFactory().with({
+			extra_args = { "-U" },
+			diagnostic_config = {
+				-- see :help vim.diagnostic.config()
+				underline = true,
+				virtual_text = false,
+				signs = false,
+				update_in_insert = false,
+				severity_sort = true,
+			},
+		}),
 
 		-- English
 		diagnostics.write_good.with({
