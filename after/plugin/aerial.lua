2cfd01bab2bfffb6344e9f0998f557e1f2549056
Yu Guo <yu.guo@mujin.co.jp>
2024-06-13 18:08:27 +0900

    use aerial navigation window

---
+++ after/plugin/aerial.lua
@@ -0,0 +1,6 @@
+function setup()
+  local aerial = require("aerial")
+  vim.keymap.set('n', '<Leader><Leader>ln', aerial.nav_toggle, { desc = "Toggle aerial navigation window" })
+end
+
+try(setup, vim.notify)
