10115decc51adc8b923ca96793c593a3d1494f65
Yu Guo <yu.guo@mujin.co.jp>
2024-06-24 17:11:25 +0900

    use neogen for generating annotations

---
+++ lua/lazy/plugin/neogen.lua
@@ -0,0 +1,15 @@
+-- TODO: Integrate annotation into none-ls
+-- see https://github.com/nvimtools/none-ls.nvim/blob/main/doc/MAIN.md
+return {
+  "danymat/neogen",
+  config = function()
+    require('neogen').setup({ snippet_engine = "luasnip" })
+    vim.keymap.set(
+      { "n", },
+      "<leader><leader>d", require('neogen').generate,
+      { desc = "generate annotations for the current function.", }
+    )
+  end,
+  -- Uncomment next line if you want to follow only stable versions
+  version = "*"
+}
