ddaab096ded48d5c5fdb5978a80b359c34597df8
Yu Guo <yu.guo@mujin.co.jp>
2024-07-09 16:05:58 +0900

    add line count for folding

--- lua/lazy/plugin/nvim-ufo.lua
+++ lua/lazy/plugin/nvim-ufo.lua
@@ -3,6 +3,35 @@ vim.opt.foldlevel = 99   -- Using ufo provider need a large value, feel free to 
 vim.opt.foldlevelstart = 99
 vim.opt.fml = 2
 
+-- see at https://github.com/kevinhwang91/nvim-ufo?tab=readme-ov-file
+local handler = function(virtText, lnum, endLnum, width, truncate)
+  local newVirtText = {}
+  local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
+  local curWidth = 0
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      local hlGroup = chunk[2]
+      table.insert(newVirtText, { chunkText, hlGroup })
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      -- str width returned from truncate() may less than 2nd argument, need padding
+      if curWidth + chunkWidth < targetWidth then
+        suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+      end
+      break
+    end
+    curWidth = curWidth + chunkWidth
+  end
+  table.insert(newVirtText, { suffix, 'MoreMsg' })
+  return newVirtText
+end
+
 return {
   'kevinhwang91/nvim-ufo',
   dependencies = { 'kevinhwang91/promise-async' },
@@ -10,7 +39,11 @@ return {
     require('ufo').setup({
       provider_selector = function(bufnr, filetype, buftype)
         return { 'treesitter', 'indent' }
-      end
+      end,
+      -- global handler
+      -- `handler` is the 2nd parameter of `setFoldVirtTextHandler`,
+      -- check out `./lua/ufo.lua` and search `setFoldVirtTextHandler` for detail.
+      fold_virt_text_handler = handler
     })
   end
 }
