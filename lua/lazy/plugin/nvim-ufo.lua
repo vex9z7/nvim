5cc49ad972124b10c2bd85ea38c6026ab5665855
Yu Guo <yu.guo@mujin.co.jp>
2024-07-09 16:04:23 +0900

    add folder configs to nvim-ufo

--- lua/lazy/plugin/nvim-ufo.lua
+++ lua/lazy/plugin/nvim-ufo.lua
@@ -1,3 +1,8 @@
+vim.opt.foldcolumn = '1' -- '0' is not bad
+vim.opt.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+vim.opt.foldlevelstart = 99
+vim.opt.fml = 2
+
 return {
   'kevinhwang91/nvim-ufo',
   dependencies = { 'kevinhwang91/promise-async' },
