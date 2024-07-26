56dd8401d36bb2dd3618f4c010cd5b0df1f4e65f
Yu Guo <yu.guo@mujin.co.jp>
2024-07-26 13:35:11 +0900

    cmp: remove enter as a confirm keybinding

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,53 +1,39 @@
 local function setup()
-	local luasnip = require("luasnip")
-	local cmp = require("cmp")
+  local luasnip = require("luasnip")
+  local cmp = require("cmp")
 
-	cmp.setup({
+  cmp.setup({
 
-		-- ... Your other configuration ...
+    -- ... Your other configuration ...
 
-		mapping = {
+    mapping = {
 
-			-- ... Your other mappings ...
-			["<CR>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					if luasnip.expandable() then
-						luasnip.expand()
-					else
-						cmp.confirm({
-							select = true,
-						})
-					end
-				else
-					fallback()
-				end
-			end),
+      -- ... Your other mappings ...
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.locally_jumpable(1) then
+          luasnip.jump(1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
 
-			["<Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item()
-				elseif luasnip.locally_jumpable(1) then
-					luasnip.jump(1)
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.locally_jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
 
-			["<S-Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				elseif luasnip.locally_jumpable(-1) then
-					luasnip.jump(-1)
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
+      -- ... Your other mappings ...
+    },
 
-			-- ... Your other mappings ...
-		},
-
-		-- ... Your other configuration ...
-	})
+    -- ... Your other configuration ...
+  })
 end
 
 try(setup, vim.notify)
