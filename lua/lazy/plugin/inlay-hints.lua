488c6d3975fa662838d356c5ef14d90210966643
Yu Guo <yu.guo@mujin.co.jp>
2024-08-29 10:57:48 +0900

    inlay-hints: disable inlay-hints to fix nosiy types in TS

--- lua/lazy/plugin/inlay-hints.lua
+++ lua/lazy/plugin/inlay-hints.lua
@@ -1,9 +1,9 @@
 -- use this simple plugin until inlay configs are merged into lsp-config
 return {
-	"MysticalDevil/inlay-hints.nvim",
-	event = "LspAttach",
-	dependencies = { "neovim/nvim-lspconfig" },
-	config = function()
-		require("inlay-hints").setup()
-	end,
+  "MysticalDevil/inlay-hints.nvim",
+  event = "LspAttach",
+  dependencies = { "neovim/nvim-lspconfig" },
+  config = function()
+    -- require("inlay-hints").setup()
+  end,
 }
