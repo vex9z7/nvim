7fbe6d7e129f022a899925516600ce2a80ca1136
Yu Guo <yu.guo@mujin.co.jp>
2024-08-30 17:37:06 +0900

    mini.animate: improve config

--- after/plugin/mini_animate.lua
+++ after/plugin/mini_animate.lua
@@ -1,5 +1,14 @@
 local function setup()
-  require('mini.animate').setup()
+  local animate = require('mini.animate')
+  animate.setup({
+    cursor = {
+      timing = animate.gen_timing.exponential({ duration = 80, unit = 'total' })
+    },
+    scroll = {
+      timing = animate.gen_timing.exponential({ duration = 80, unit = 'total' }),
+      subscroll = animate.gen_subscroll.equal({ max_output_steps = 20}),
+    }
+  })
 end
 
 try(setup, vim.notify)
