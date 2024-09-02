9e34e0f9b181f87e8456dad7205b8dff785f1d93
Yu Guo <yu.guo@mujin.co.jp>
2024-09-02 18:22:37 +0900

    lorem.nvim: Initial config

---
+++ after/plugin/lorem.lua
@@ -0,0 +1,12 @@
+local function setup()
+  local lorem = require('lorem')
+  lorem.setup({
+    sentenceLength = "mixed",
+    comma_chance = 0.2,
+    max_commas_per_sentence = 2,
+  })
+
+  vim.keymap.set('n', '<leader><leader>L', '<CMD>LoremIpsum 1 paragraphs<CR>', { noremap = true })
+end
+
+try(setup, vim.notify)
---
+++ lua/lazy/plugin/lorem.lua
@@ -0,0 +1,6 @@
+return {
+  'derektata/lorem.nvim',
+  commit = "b1cb57be4a39b4a833fca7679f9caec9c8d8f183",
+  config = function()
+  end
+}
