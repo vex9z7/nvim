395553adcf3f114cce09f6d31e6aa59c1c517bfd
vex9z7 <face.tubas0p@icloud.com>
2024-10-24 17:42:33 +0900

    try returns result when succeeds, fallback when fails

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,7 +1,12 @@
 -- see https://www.lua.org/gems/lpg113.pdf
-function try(f, catch_f)
-	local status, exception = pcall(f)
-	if not status then
-		catch_f(exception)
-	end
+function try(f, on_fail, fallback)
+  local success, result = pcall(f)
+  if success then
+    return result
+  else
+    if on_fail ~= nil then
+      on_fail(result)
+    end
+    return fallback
+  end
 end
