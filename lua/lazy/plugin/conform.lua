ca58e13ddcd7ef6cadfb00b73fae57f88c90c944
vex9z7 <face.tubas0p@icloud.com>
2024-11-21 15:36:19 +0900

    conform: support toggle auto format

--- lua/lazy/plugin/conform.lua
+++ lua/lazy/plugin/conform.lua
@@ -21,11 +21,41 @@ return {
 				yaml = { "prettierd" },
 				markdown = { "prettierd" },
 			},
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
+
+			-- Toggle auto format
+			-- see https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
+			format_on_save = function(bufnr)
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return {
+					-- These options will be passed to conform.format()
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				}
+			end,
+		})
+
+		vim.keymap.set({ "v", "n" }, "<leader><leader>uf", function()
+			vim.b.disable_autoformat = not vim.b.disable_autoformat
+			if vim.b.disable_autoformat then
+				vim.print("auto formatter disabled (buffer)")
+			else
+				vim.print("auto formatter enabled (buffer)")
+			end
+		end, {
+			desc = "Toggle autoformatting (buffer)",
+		})
+
+		vim.keymap.set({ "v", "n" }, "<leader><leader>uF", function()
+			vim.g.disable_autoformat = not vim.g.disable_autoformat
+			if vim.g.disable_autoformat then
+				vim.print("auto formatter disabled (global)")
+			else
+				vim.print("auto formatter enabled (global)")
+			end
+		end, {
+			desc = "Toggle autoformatting (global)",
 		})
 
 		require("mason-conform").setup()
