e7ace870eb7416bd580e598cfe21925b02197ae5
vex9z7 <face.tubas0p@icloud.com>
2024-11-21 16:08:09 +0900

    conform: refactor

--- lua/lazy/plugin/conform.lua
+++ lua/lazy/plugin/conform.lua
@@ -1,10 +1,10 @@
 return {
 	"stevearc/conform.nvim",
-	dependencies = {
-		"williamboman/mason.nvim",
-		"zapling/mason-conform.nvim",
-	},
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
 	keys = {
+		-- Support toggling auto formating
+		-- see https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
 		{
 			"<leader><leader>uf",
 			function()
@@ -32,40 +32,34 @@ return {
 			desc = "Toggle autoformatting (global)",
 		},
 	},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				-- Conform will run multiple formatters sequentially
-				lua = { "stylua" },
-				python = { "ruff_fix", "ruff_organize_imports", "ruff_format" },
-				rust = { "rustfmt" },
-				javascript = { "eslint_d", "prettierd" },
-				javascriptreact = { "eslint_d", "prettierd" },
-				typescript = { "eslint_d", "prettierd" },
-				typescriptreact = { "eslint_d", "prettierd" },
-				css = { "stylelint", "prettierd" },
-				less = { "stylelint", "prettierd" },
-				json = { "prettierd" },
-				yaml = { "prettierd" },
-				markdown = { "prettierd" },
-			},
-
-			-- Toggle auto format
-			-- see https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
-			format_on_save = function(bufnr)
-				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-					return
-				end
-				return {
-					-- These options will be passed to conform.format()
-					timeout_ms = 500,
-				}
-			end,
-			default_format_opts = {
-				lsp_format = "fallback",
-			},
-		})
-
-		require("mason-conform").setup()
-	end,
+	-- This will provide type hinting with LuaLS
+	---@module "conform"
+	---@type conform.setupOpts
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			-- Conform will run multiple formatters sequentially
+			lua = { "stylua" },
+			python = { "ruff_fix", "ruff_organize_imports", "ruff_format" },
+			rust = { "rustfmt" },
+			javascript = { "eslint_d", "prettierd" },
+			javascriptreact = { "eslint_d", "prettierd" },
+			typescript = { "eslint_d", "prettierd" },
+			typescriptreact = { "eslint_d", "prettierd" },
+			css = { "stylelint", "prettierd" },
+			less = { "stylelint", "prettierd" },
+			json = { "prettierd" },
+			yaml = { "prettierd" },
+			markdown = { "prettierd" },
+		},
+		default_format_opts = { lsp_format = "fallback" },
+		-- Set up format-on-save
+		format_on_save = function(bufnr)
+			-- Disable with a global or buffer-local variable
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+			return { timeout_ms = 500, lsp_format = "fallback" }
+		end,
+	},
 }
